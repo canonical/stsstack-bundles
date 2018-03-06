dbe45fd9e58ff2d2bfeb70141f193d8c01db5932
Edward Hope-Morley <edward.hope-morley@canonical.com>
2018-03-06 14:29:13 +0000

    [multi-az] fix tool

--- tools/create_nova_az_aggregates.sh
+++ tools/create_nova_az_aggregates.sh
@@ -2,9 +2,11 @@
 for az in az1 az2; do
     readarray ids<<<"`juju status nova-compute-$az --format=yaml| grep instance-id| awk '{print $2}'`"
     machines=()
-    for id in ${ids[@]}; do machines+=( `openstack server show $id| grep " name "| awk '{print $4}'` ); done
-    nova aggregate-details ${az^^} &>/dev/null || nova aggregate-create ${az^^} $az;    
+    for id in ${ids[@]}; do machines+=( `source ~/novarc; openstack server show $id| grep " name "| awk '{print $4}'` ); done
+    echo "Creating aggregate ${az^^}"
+    openstack aggregate show ${az^^} &>/dev/null || openstack aggregate create --zone $az ${az^^};
     for m in ${machines[@]}; do
+        echo "Adding host $m to aggregate ${az^^}"
         openstack aggregate show ${az^^}| grep -q $m || openstack aggregate add host ${az^^} $m
     done
 done
