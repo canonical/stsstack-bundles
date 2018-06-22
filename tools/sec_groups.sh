1be1de9051f23a8c280ce7f79776e859da55125c
Edward Hope-Morley <edward.hope-morley@canonical.com>
2018-06-22 16:06:26 +0100

    Add v3 domain support to sec_groups tool

--- tools/sec_groups.sh
+++ tools/sec_groups.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -ex
 # Add sec groups for basic access
-secgroup=${1:-`openstack security group list --project admin| grep default| awk '{print $2}'`}
+`env| egrep -q "^OS_.*DOMAIN.*|/v3"` && v3args="--project-domain admin_domain" || v3args=""
+secgroup=${1:-`openstack security group list --project admin $v3args| grep default| awk '{print $2}'`}
 for port in 22 53 80 443; do
-    openstack security group rule create $secgroup --protocol tcp --remote-ip 0.0.0.0/0 --dst-port $port --project admin ||:
+    openstack security group rule create $secgroup --protocol tcp --remote-ip 0.0.0.0/0 --dst-port $port --project admin $v3args ||:
 done
 
-openstack security group rule create $secgroup --protocol icmp --remote-ip 0.0.0.0/0 --project admin ||:
+openstack security group rule create $secgroup --protocol icmp --remote-ip 0.0.0.0/0 --project admin $v3args ||:
