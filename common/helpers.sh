e2cf95d0395994af5df89fcd8f37331fa9b203f2
Edward Hope-Morley <edward.hope-morley@canonical.com>
2019-01-10 12:22:27 +0000

    Improve help message

--- common/helpers.sh
+++ common/helpers.sh
@@ -2,7 +2,44 @@ declare -A parameters=()
 declare -a overlays=()
 
 _usage () {
-echo "USAGE: `basename $0` [--name n] [--series s] [--release r] [--pocket p] [--replay] --template t --path p $@"
+cat << EOF
+USAGE: `basename $0` INTERNAL_OPTS OPTIONS
+
+OPTIONS:
+     --create-model
+        Create Juju model using --name. Switches to model if it already
+        exists. If this is not provided then the current Juju model is used.
+     -h, --help
+        Display this help message.
+     --list
+        List existing bundles.
+     -n, --name n
+        Name for bundle. If this is not provided then the default bundle
+        location is used.
+     -p, --pocket p
+        Archive pocket to install packages from e.g. "proposed".
+     -r, --release r
+        Openstack release. This allows UCA to be used otherwise base archive
+        of release is used.
+     --replay
+        Replay last command for bundle --name (or default bundle if no name
+        provided).
+     --run
+        Automatically execute the generated deployment command.
+     -s, --series s
+        Ubuntu series.
+
+INTERNAL_OPTS (don't use these):
+     --bundle-params
+        (internal only) Bundle paramaters passed by sub-generator
+     --overlay p
+        (internal only) Overlay to be added to deployment. Can be
+        specified multiple times.
+     --path p
+        (internal only) Target bundle directory
+     -t, --template t
+        (internal only) Generated bundle templates.
+EOF
 }
 
 get_units()
