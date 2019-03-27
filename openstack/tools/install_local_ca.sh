5a05d737dd61fee08c2f0cd2c1a16d902786c960
Edward Hope-Morley <edward.hope-morley@canonical.com>
2019-03-27 13:40:43 +0000

    Put back sudo update-ca-certificates --fresh
    Accidentally removed from previous commit.

--- openstack/tools/install_local_ca.sh
+++ openstack/tools/install_local_ca.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -eux
 sudo cp ssl/results/cacert.pem /usr/local/share/ca-certificates/cacert.crt
 sudo chmod 644 /usr/local/share/ca-certificates/cacert.crt
+sudo update-ca-certificates --fresh
