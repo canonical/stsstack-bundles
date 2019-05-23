79fb945fa1b02fd23919a1e5e4cbe54627268a84
Edward Hope-Morley <edward.hope-morley@canonical.com>
2019-05-23 10:30:41 +0100

    Fix bin/add-data-ports.sh
    Only ever add one extra port i.e. there must only be one data-port
    from this network otherwise it creates a network loop.

--- openstack/bin/add-data-ports.sh
+++ openstack/bin/add-data-ports.sh
@@ -1,19 +1,51 @@
 #!/bin/bash -eu
+#
+# Create a new port (unless one already exists) and add to each
+# nova-compute unit host such each unit has one (and only one)
+# extra port.
+#
 application=${1:-nova-compute}  # note: can't be a subordinate
 . ~/novarc
 readarray -t instances<<<"`juju status $application --format=json| jq -r '.machines[].\"instance-id\"'`"
-num_instances=${#instances[@]}
-readarray -t ports<<<"`openstack port list| grep data-port| grep DOWN| awk '{print $2}'`"
-[ -n "${ports[0]}" ] || ports=()
-((${#ports[@]})) && delta=$((($num_instances - ${#ports[@]}) % $num_instances)) || delta=$num_instances
-echo "Creating $delta new ports" 
-for ((i=0;i<$delta;i++)); do
-    openstack port create data-port --network ${OS_PROJECT_NAME}_admin_net
-done
-readarray -t ports<<<"`openstack port list| grep data-port| grep DOWN| awk '{print $2}'`"
-i=0
+
+declare -A requires=()
+require_count=0
+echo "Checking nova-compute units: ${instances[@]}"
 for inst in "${instances[@]}"; do
-    port=${ports[$((i++))]}
-    echo "Adding port $port to server $inst"
-    openstack server add port $inst $port
+    num_ports="`openstack port list --server $inst| grep ACTIVE| wc -l`"
+    if ((num_ports>1)); then
+        requires[$inst]=false
+    else
+        requires[$inst]=true
+        ((require_count+=1))
+    fi
 done
+
+if ((require_count>0)); then
+    # Get extant free ports
+    readarray -t ports<<<"`openstack port list| grep data-port| grep DOWN| awk '{print $2}'`"
+    [ -n "${ports[0]}" ] || ports=()
+    if ((${#ports[@]})); then
+        delta=$((($require_count - ${#ports[@]}) % $require_count))
+    else
+        delta=$require_count
+    fi
+
+    echo "Creating $delta new ports" 
+    for ((i=0;i<$delta;i++)); do
+        openstack port create data-port --network ${OS_PROJECT_NAME}_admin_net
+    done
+
+    readarray -t ports<<<"`openstack port list| grep data-port| grep DOWN| awk '{print $2}'`"
+    i=0
+    for inst in "${instances[@]}"; do
+        ${requires[$inst]} || continue
+        port=${ports[$((i++))]}
+        echo "Adding port $port to server $inst"
+        openstack server add port $inst $port
+    done
+else
+    echo "All instances have > 1 port already - nothing to do"
+fi
+
+echo "Done."
