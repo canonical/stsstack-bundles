8ab472c328659c7c17a56394717a801937b7fe33
Edward Hope-Morley <edward.hope-morley@canonical.com>
2019-06-26 12:59:00 +0100

    Add tools to power on/off all vms in a model

---
+++ tools/model-poweroff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+model=$1
+
+(($#)) || { echo "You must provide a model name" && exit 1; }
+
+echo -e "Power off all instances for model '$model'\nContinue? [y/N] "
+read answer
+[ "${answer,,}" = "y" ] || { echo "aborted"; exit 0; }
+
+source ~/novarc
+echo "Fetching vms for model..."
+readarray -t vms<<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-.+"| awk '{print $1}'`"
+echo "Powering off ${#vms[@]} vms"
+openstack server stop ${vms[@]}
+echo "Done."
---
+++ tools/model-poweron.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+model=$1
+
+(($#)) || { echo "You must provide a model name" && exit 1; }
+
+echo -e "Power on all instances for model '$model'\nContinue? [y/N] "
+read answer
+[ "${answer,,}" = "y" ] || { echo "aborted"; exit 0; }
+
+source ~/novarc
+echo "Fetching vms for model..."
+readarray -t vms<<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-.+"| awk '{print $1}'`"
+echo "Powering on ${#vms[@]} vms"
+openstack server start ${vms[@]}
+echo "Done."
