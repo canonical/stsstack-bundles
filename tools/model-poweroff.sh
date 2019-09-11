2d861f96303e12222f2488230bf83049cc8328ca
Edward Hope-Morley <edward.hope-morley@canonical.com>
2019-09-11 09:45:00 +0100

    Make model stop/start script clearer

--- tools/model-poweroff.sh
+++ tools/model-poweroff.sh
@@ -3,13 +3,14 @@ model=$1
 
 (($#)) || { echo "You must provide a model name" && exit 1; }
 
-echo -e "Power off all instances for model '$model'\nContinue? [y/N] "
+source ~/novarc
+echo "Fetching vms for model '${model}'..."
+readarray -t vms<<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-[0-9]+"| awk '{print $1}'`"
+
+echo -e "Power off ${#vms[@]} instances for model '${model}'\nContinue? [y/N] "
 read answer
 [ "${answer,,}" = "y" ] || { echo "aborted"; exit 0; }
 
-source ~/novarc
-echo "Fetching vms for model..."
-readarray -t vms<<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-[0-9]+"| awk '{print $1}'`"
-echo "Powering off ${#vms[@]} vms"
+echo "Stopping vms: ${vms[@]}"
 openstack server stop ${vms[@]}
 echo "Done."
--- tools/model-poweron.sh
+++ tools/model-poweron.sh
@@ -3,13 +3,14 @@ model=$1
 
 (($#)) || { echo "You must provide a model name" && exit 1; }
 
-echo -e "Power on all instances for model '$model'\nContinue? [y/N] "
+source ~/novarc
+echo "Fetching vms for model '${model}'..."
+readarray -t vms<<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-[0-9]+"| awk '{print $1}'`"
+
+echo -e "Power on ${#vms[@]} instances for model '${model}'\nContinue? [y/N] "
 read answer
 [ "${answer,,}" = "y" ] || { echo "aborted"; exit 0; }
 
-source ~/novarc
-echo "Fetching vms for model..."
-readarray -t vms<<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-[0-9]+"| awk '{print $1}'`"
-echo "Powering on ${#vms[@]} vms"
+echo "Starting ${#vms[@]} vms"
 openstack server start ${vms[@]}
 echo "Done."
