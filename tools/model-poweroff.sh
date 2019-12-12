97f2f02fba6bf1cf66bf879604b1aefdf0d130a1
Edward Hope-Morley <edward.hope-morley@canonical.com>
2019-12-12 21:15:09 +0000

    model poweron/off don't newline after question

--- tools/model-poweroff.sh
+++ tools/model-poweroff.sh
@@ -7,7 +7,7 @@ source ~/novarc
 echo "Fetching vms for model '${model}'..."
 readarray -t vms<<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-[0-9]+"| awk '{print $1}'`"
 
-echo -e "Power off ${#vms[@]} instances for model '${model}'\nContinue? [y/N] "
+echo -en "Power off ${#vms[@]} instances for model '${model}'\nContinue? [y/N]"
 read answer
 [ "${answer,,}" = "y" ] || { echo "aborted"; exit 0; }
 
