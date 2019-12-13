abc0856b35f5dc33cb958e0ee951fc0f83d9edf5
Edward Hope-Morley <edward.hope-morley@canonical.com>
2019-12-13 10:18:31 +0000

    Update install_local_ca.sh to support Vault CA
    Defaults to using stsstack-bundles generated CA but
    now optionally allows fetching CA cert from deployed
    Vault.

--- openstack/tools/install_local_ca.sh
+++ openstack/tools/install_local_ca.sh
@@ -1,4 +1,19 @@
 #!/bin/bash -eux
-sudo cp ssl/openstack/results/cacert.pem /usr/local/share/ca-certificates/cacert.crt
+local_ca_crt_path=${1:-ssl/openstack/results/cacert.pem}
+use_vault=${2:-false}
+
+ftmp=`mktemp`
+
+cleanup () { rm -f $ftmp; }
+trap cleanup EXIT INT
+
+if $use_vault || ! [ -e "$local_ca_crt_path" ]; then
+    echo "Fetching CA cert from vault"
+    juju run-action --format=json vault/leader get-root-ca --wait | jq -r .[].results.output > $ftmp
+    local_ca_crt_path=$ftmp
+fi
+
+echo "INFO: installing stsstack-bundles openstack CA at /usr/local/share/ca-certificates/cacert.crt"
+sudo cp ${local_ca_crt_path} /usr/local/share/ca-certificates/cacert.crt
 sudo chmod 644 /usr/local/share/ca-certificates/cacert.crt
 sudo update-ca-certificates --fresh
