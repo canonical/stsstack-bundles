4cd5ffdc1dbacf4bef94849054a04a902936eb9f
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-01-22 16:56:42 +0000

    Fix tools/upload_octavia_image.sh
    Requires Openstack name.

--- openstack/tools/upload_octavia_amphora_image.sh
+++ openstack/tools/upload_octavia_amphora_image.sh
@@ -1,12 +1,17 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 # Upload amphora image from stsstack swift to overcloud glance.
-source ~/novarc
+release=${1:-""}
+if [ -z "$release" ]; then
+    read -p "Openstack release name: " release
+fi
 
+set -x
+source ~/novarc
 tmp=`mktemp`
 source ~/novarc
 swift list images > $tmp
-ts="`sed -r 's/.+-([[:digit:]]+)\.[[:alnum:]]+$/\1/g;t;d' $tmp| sort -h| tail -n 1`"
-img="`grep $ts $tmp| tail -n 1`"
+ts=`sed -r "s/.+-([[:digit:]]+)-$release.+/\1/g;t;d" $tmp| sort -h| tail -n 1`
+img="`egrep "${ts}.+$release" $tmp| tail -n 1`"
 rm $tmp
 
 export SWIFT_IP="10.230.19.58"
