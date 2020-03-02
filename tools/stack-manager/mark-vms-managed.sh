1c6220ec9fae5f96d91f8c376d039a485b947d79
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-03-02 15:36:54 +0000

    Add missing stack-manager tools from prev commit.

---
+++ tools/stack-manager/mark-vms-managed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -u
+. `dirname $0`/common.sh
+vms=( $@ )
+
+(("${#vms[@]}")) || { echo "ERROR: no vms provided"; exit 1; }
+
+openstack object save stack-manager-${OS_PROJECT_NAME} --file $STAGING_DIR/managed_vms.json managed_vms.json
+echo "Marking ${#vms[@]} vms as managed=true"
+cp $STAGING_DIR/managed_vms.json $STAGING_DIR/managed_vms.json.updated
+for vm in ${vms[@]}; do
+    jq "(.catalog[].servers)|=(map((if .id|test(\"$vm\") then .managed=\"true\" else . end)))" $STAGING_DIR/managed_vms.json.updated > $STAGING_DIR/managed_vms.json.updated.tmp
+    cp $STAGING_DIR/managed_vms.json.updated.tmp $STAGING_DIR/managed_vms.json.updated
+done
+diff $STAGING_DIR/managed_vms.json $STAGING_DIR/managed_vms.json.updated &>/dev/null
+if (($?>0)); then
+    openstack object create stack-manager-${OS_PROJECT_NAME} --name managed_vms.json $STAGING_DIR/managed_vms.json.updated
+else
+    echo "no changes applied"
+fi
+echo "Done."
