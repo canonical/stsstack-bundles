cf19998ff3f842073b606f525d11b956b85ac546
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-03-11 20:23:01 +0000

    Switch to configuring VIPs dynamically
    Allows the network from which vips are allocated to be
    changed as provided by a new config option --vip-network.

--- ssl/create_ca_cert.sh
+++ ssl/create_ca_cert.sh
@@ -1,15 +1,29 @@
 #!/bin/bash -eu
-module=${1:-default}
-results_dir=${module}/results
+state_dir=$1
+results_dir=${state_dir}/results
+
+echo "Generating ssl certificates in ssl/$state_dir"
+
 #rm -rf $results_dir
 mkdir -p $results_dir
-sed -r "s,__RESULTS_PATH__,$results_dir,g" openssl-ca.cnf.template > $module/openssl-ca.cnf
-sed -r "s,__RESULTS_PATH__,$results_dir,g" openssl-server.cnf.template > $module/openssl-server.cnf
+sed -r "s,__RESULTS_PATH__,$results_dir,g" openssl-ca.cnf.template > ${state_dir}/openssl-ca.cnf
+sed -r "s,__RESULTS_PATH__,$results_dir,g" openssl-server.cnf.template > ${state_dir}/openssl-server.cnf
+
+sed -i -r "s,__CN_VIP__,${MASTER_OPTS[VIP_ADDR_START]},g" $state_dir/openssl-server.cnf
+vip_net=${MASTER_OPTS[VIP_ADDR_START]}
+vip_net_prefix=${vip_net%\.*}
+vip_net_suffix=${vip_net##*\.}
+
+# TODO: figure out exactly how many we need
+for ((i=0;i<20;i++)); do
+    echo "IP.$((i+1))  = $vip_net_prefix.$((vip_net_suffix+i))" >> $state_dir/openssl-server.cnf
+done
+
 touch $results_dir/index.txt
 echo '01' > $results_dir/serial.txt
 [ -r "$results_dir/cacert.pem" ] || \
-    openssl req -x509 -config $module/openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out $results_dir/cacert.pem -outform PEM -subj "/C=GB/ST=England/L=London/O=Ubuntu Cloud/OU=Cloud"
+    openssl req -x509 -config $state_dir/openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out $results_dir/cacert.pem -outform PEM -subj "/C=GB/ST=England/L=London/O=Ubuntu Cloud/OU=Cloud" &>/dev/null
 [ -r "$results_dir/servercert.csr" ] || \
-    openssl req -config $module/openssl-server.cnf -newkey rsa:2048 -sha256 -nodes -out $results_dir/servercert.csr -outform PEM -subj "/C=GB/ST=England/L=London/O=Ubuntu Cloud/OU=Cloud/CN=10.5.100.1"
+    openssl req -config $state_dir/openssl-server.cnf -newkey rsa:2048 -sha256 -nodes -out $results_dir/servercert.csr -outform PEM -subj "/C=GB/ST=England/L=London/O=Ubuntu Cloud/OU=Cloud/CN=${MASTER_OPTS[VIP_ADDR_START]}" &>/dev/null
 [ -r "$results_dir/servercert.pem" ] || \
-    openssl ca -batch -config $module/openssl-ca.cnf -policy signing_policy -extensions signing_req -out $results_dir/servercert.pem -infiles $results_dir/servercert.csr
+    openssl ca -batch -config $state_dir/openssl-ca.cnf -policy signing_policy -extensions signing_req -out $results_dir/servercert.pem -infiles $results_dir/servercert.csr &>/dev/null
