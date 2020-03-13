4494493614d35b4e623fd1c56cbba195cc2b0ec1
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-03-13 14:06:46 +0000

    Dont clobber existing ssl certs
    When using generated certs (for config-based ssl) the results
    are stored on a named directory. If we re-run the same bundle
    we dont want the certs to change so prevent this from
    happening.

--- ssl/create_ca_cert.sh
+++ ssl/create_ca_cert.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -eu
 state_dir=$1
 results_dir=${state_dir}/results
+declare -a required=( cacert.pem servercert.csr servercert.pem )
 
-echo "Generating ssl certificates in ssl/$state_dir"
+all_exist=true
+for f in ${required[@]}; do
+    [ -r "$results_dir/$f" ] || all_exist=false && break
+done
 
-#rm -rf $results_dir
+if $all_exist; then
+    echo -e "Using existing ssl certificates in ssl/$state_dir\n"
+    exit
+else
+    echo -e "Generating ssl certificates in ssl/$state_dir\n"
+fi
+
 mkdir -p $results_dir
 sed -r "s,__RESULTS_PATH__,$results_dir,g" openssl-ca.cnf.template > ${state_dir}/openssl-ca.cnf
 sed -r "s,__RESULTS_PATH__,$results_dir,g" openssl-server.cnf.template > ${state_dir}/openssl-server.cnf
@@ -21,9 +31,8 @@ done
 
 touch $results_dir/index.txt
 echo '01' > $results_dir/serial.txt
-[ -r "$results_dir/cacert.pem" ] || \
-    openssl req -x509 -config $state_dir/openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out $results_dir/cacert.pem -outform PEM -subj "/C=GB/ST=England/L=London/O=Ubuntu Cloud/OU=Cloud" &>/dev/null
-[ -r "$results_dir/servercert.csr" ] || \
-    openssl req -config $state_dir/openssl-server.cnf -newkey rsa:2048 -sha256 -nodes -out $results_dir/servercert.csr -outform PEM -subj "/C=GB/ST=England/L=London/O=Ubuntu Cloud/OU=Cloud/CN=${MASTER_OPTS[VIP_ADDR_START]}" &>/dev/null
-[ -r "$results_dir/servercert.pem" ] || \
-    openssl ca -batch -config $state_dir/openssl-ca.cnf -policy signing_policy -extensions signing_req -out $results_dir/servercert.pem -infiles $results_dir/servercert.csr &>/dev/null
+{
+openssl req -x509 -config $state_dir/openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out $results_dir/cacert.pem -outform PEM -subj "/C=GB/ST=England/L=London/O=Ubuntu Cloud/OU=Cloud"
+openssl req -config $state_dir/openssl-server.cnf -newkey rsa:2048 -sha256 -nodes -out $results_dir/servercert.csr -outform PEM -subj "/C=GB/ST=England/L=London/O=Ubuntu Cloud/OU=Cloud/CN=${MASTER_OPTS[VIP_ADDR_START]}"
+openssl ca -batch -config $state_dir/openssl-ca.cnf -policy signing_policy -extensions signing_req -out $results_dir/servercert.pem -infiles $results_dir/servercert.csr
+} &>/dev/null
