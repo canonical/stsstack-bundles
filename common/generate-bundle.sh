acf742a2c6f8770f8e14d74e3f6673f87baab341
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-03-13 12:57:07 +0000

    Reorganise how we manage config defaults
    This is a major refactor/reorg to accomodate for having
    per-module config defaults that only need be defined
    once and can be imported by other modules. This avoids
    duplicate definitions and allows overrides if
    necessary. To enable this, defaults are now defined in
    a module_defaults file that exists in each module path
    and are imported as part of the generate pipeline and
    module dependencies (i.e. default from other modules)
    can be defined in each module's 01import-config-defaults.
    
    Another nice side-effect is that we can simplify overlays
    by only having num_units set for principle definitions
    e.g. *-ha templates no longer need to set num_units for
    override definitions.
    
    Also adds a new lint tool for checking if any overlays
    are missing a default definition and adds an automatic
    check to generate-bundle.sh to ensure that generated
    bundles/overlays don't contain any dangling/unrendered
    variables.

--- common/generate-bundle.sh
+++ common/generate-bundle.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -eu
 LIB_COMMON=`dirname $0`/common
 . $LIB_COMMON/helpers
-. pipeline/01setup
+. pipeline/00setup
+. pipeline/01import-config-defaults
 . pipeline/02configure
 . pipeline/03build
+# Ensure no unrendered variables
+out="`grep -r __ b/${MASTER_OPTS[BUNDLE_NAME]} --exclude=config`"
+[ -n "$out" ] || exit
+echo -e "ERROR: there are unrendered variables in your bundle:\n$out"
