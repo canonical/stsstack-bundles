43e0a9007f782036f1f06a068fa7eb8abddbb855
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-05-27 12:39:19 +0100

    Fixup newline issue in tools/model-poweron.sh

--- tools/model-poweron.sh
+++ tools/model-poweron.sh
@@ -7,8 +7,6 @@ source ~/novarc
 echo "Fetching vms for model '${model}'..."
 readarray -t vms<<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-[0-9]+"| awk '{print $1}'`"
 
-echo -en "Power on ${#vms[@]} instances for model '${model}'"
-
-echo "Starting ${#vms[@]} vms"
+echo -e "Starting ${#vms[@]} instances for model '${model}'"
 openstack server start ${vms[@]}
 echo "Done."
