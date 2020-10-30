fb50c14e923fb4fea26408a26c10fc60345f5495
Jorge Niedbalski <jnr@metaklass.org>
2020-10-30 12:35:13 -0300

    check if model_ca_cert_path contents != None
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- openstack/tools/install_local_ca.sh
+++ openstack/tools/install_local_ca.sh
@@ -4,7 +4,7 @@ model_ca_cert_path=${1:-ssl/openstack/results/cacert.pem}
 if ((`juju status --format=json| jq -r '.applications[]| select(."charm-name"=="vault")'| wc -l`)); then
     model_uuid=`juju show-model --format=json| jq -r '.[]."model-uuid"'`
     model_ca_cert_path=`find /tmp -name \*.stsstack-bundles.ssl.$model_uuid 2>/dev/null` || true
-    if [ -z "$model_ca_cert_path" ]; then
+    if [[ -z "$model_ca_cert_path" || "$(cat $model_ca_cert_path)" = "None" ]]; then
         model_ca_cert_path=`mktemp --suffix=.stsstack-bundles.ssl.$model_uuid`
         echo "Fetching CA cert from vault"
         juju run-action --format=json vault/leader get-root-ca --wait | jq -r .[].results.output > $model_ca_cert_path
