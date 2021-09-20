184be61966cf4b04b1f99c6d49c4d0b9f03f1464
David Negreira <david.negreira@canonical.com>
2021-09-20 09:36:56 +0200

    Only try to power-on SHUTOFF VMs
    Sometimes not all machines are powered-off in a model and running the
    script returns an error due to the fact that it is trying to power-on
    a VM that is not in a 'SHUTOFF' state.
    This fix aims to improve the script so that only machines in SHUTOFF
    state in the model are powered on.
    
    Signed-off-by: David Negreira <david.negreira@canonical.com>

--- tools/model-poweron.sh
+++ tools/model-poweron.sh
@@ -5,7 +5,7 @@ model=$1
 
 source ~/novarc
 echo "Fetching vms for model '${model}'..."
-readarray -t vms<<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-[0-9]+"| awk '{print $1}'`"
+readarray -t vms<<<"`openstack server list -c ID -c Name -c Status -f value| egrep "juju-.+-${model}-[0-9]+"| grep SHUTOFF | awk '{print $1}'`"
 
 echo -e "Starting ${#vms[@]} instances for model '${model}'"
 openstack server start ${vms[@]}
