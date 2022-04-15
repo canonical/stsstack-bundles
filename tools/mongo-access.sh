1cd380e9ad1511669027ba80fbf0f4a91c785a53
Nicolas Bock <nicolas.bock@canonical.com>
2022-04-15 18:06:01 +0000

    Add command line options and help
    Issue: canonical/stsstack-bundles#25
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

--- tools/mongo-access.sh
+++ tools/mongo-access.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
-  
-machine=${1:-0}
-model=${2:-controller}
 
-read -d '' -r cmds <<'EOF'
+set -u
+
+: "${machine:=0}"
+: "${model:=controller}"
+
+while (( $# > 0 )); do
+  case $1 in
+    -h|--help)
+      cat <<EOF
+Usage:
+
+$(basename $0) [--machine ID] [--model MODEL]
+
+Options:
+
+--machine ID    The machine ID to connect to (default ${machine})
+--model MODEL   The model the machine is a part of (default ${model})
+EOF
+      exit 0
+      ;;
+    --machine)
+      shift
+      machine="$1"
+      ;;
+    --model)
+      shift
+      model="$1"
+      ;;
+  esac
+  shift
+done
+
+read -d '' cmds <<'EOF'
 conf=/var/lib/juju/agents/machine-*/agent.conf
 user=`sudo grep tag $conf | cut -d' ' -f2`
 password=`sudo grep statepassword $conf | cut -d' ' -f2`
@@ -14,6 +43,8 @@ elif [ -f /usr/lib/juju/mongo*/bin/mongo ]; then
 else
   client=/usr/bin/mongo
 fi
-$client 127.0.0.1:37017/juju --authenticationDatabase admin --ssl --sslAllowInvalidCertificates --username "$user" --password "$password"
+$client 127.0.0.1:37017/juju --authenticationDatabase admin \
+  --ssl --sslAllowInvalidCertificates \
+  --username "$user" --password "$password"
 EOF
 juju ssh -m "${model}" "${machine}" "${cmds}"
