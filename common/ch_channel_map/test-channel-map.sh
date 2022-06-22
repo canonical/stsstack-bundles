dfd99fccddf69a364ce65a00ce3968be32f33b4d
Ubuntu <ubuntu@nicolasbock-bastion.cloud.sts>
2022-06-22 11:14:56 +0000

    Add SERIES-RELEASE specific channel maps
    The charmhub introduced charm channels that are not simply OS series specific
    but also depend on the OpenStack release. This change adds SERIES-RELEASE
    specific maps.
    
    This change adds a script to print out the channel assignments for
    verification.
    
    For example
    
        $ ./common/ch_channel_map/test-channel-map.sh focal ussuri
        hacluster              2.0.3/edge
        mysql-router           8.0/edge
        mysql-innodb-cluster   8.0/edge
        ovn-central            20.03/edge
        ovn-chassis            20.03/edge
        ovn-dedicated-chassis  20.03/edge
        percona-cluster        unset
        rabbitmq-server        3.8/edge
        vault                  1.6/edge
    
    Closes: canonical/stsstack-bundles#42
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

---
+++ common/ch_channel_map/test-channel-map.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e -u
+
+series=$1
+release=$2
+
+declare -A CHARM_CHANNEL
+
+# This variable is set to avoid an "undefined" error but otherwise not used
+ceph_release=octopus
+
+SCRIPT_DIR=$(dirname $0)
+
+. ${SCRIPT_DIR}/../charm_lists
+. ${SCRIPT_DIR}/${series}
+if [[ -f ${SCRIPT_DIR}/${series}-${release} ]]; then
+  . ${SCRIPT_DIR}/${series}-${release}
+fi
+. ${SCRIPT_DIR}/any_series
+
+output=$(mktemp)
+
+for charm in \
+  hacluster \
+  mysql-{router,innodb-cluster} \
+  ovn-{central,chassis,dedicated-chassis} \
+  percona-cluster \
+  rabbitmq-server \
+  vault
+do
+  if [[ -v CHARM_CHANNEL[${charm}] ]]; then
+    echo "${charm} ${CHARM_CHANNEL[${charm}]}" >> ${output}
+  else
+    echo "${charm} unset" >> ${output}
+  fi
+done
+cat ${output} | column --table
+rm ${output}
