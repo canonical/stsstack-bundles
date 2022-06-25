1d9ec4c0afbe1ed4b8a52c35d9f75c9141c82bf8
Nicolas Bock <nicolas.bock@canonical.com>
2022-06-25 23:05:31 +0200

    Add bashate linting to CI
    First commit lints only `tools/*.sh`.
    
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

--- tools/model-poweroff.sh
+++ tools/model-poweroff.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -e
 model=$1
 
-(($#)) || { echo "You must provide a model name" && exit 1; }
+if (($# == 0)); then
+    echo "You must provide a model name"
+    exit 1
+fi
 
 source ~/novarc
 echo "Fetching vms for model '${model}'..."
-readarray -t vms<<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-[0-9]+"| awk '{print $1}'`"
+readarray -t vms <<<"`openstack server list -c ID -c Name -f value| egrep "juju-.+-${model}-[0-9]+"| awk '{print $1}'`"
 
 echo -en "Power off ${#vms[@]} instances for model '${model}'\nContinue? [y/N]"
 read answer
