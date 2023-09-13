e23969187734d3b301bd5b0217a23b7470e2c356
Nicolas Bock <nicolas.bock@canonical.com>
2023-09-13 10:10:31 -0600

    Fix channel map for pacemaker-remote
    * Fix channel map for pacemaker-remote
    * Fix testing script for channel maps
    * Update bash linter
    
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

--- common/ch_channel_map/test-all.sh
+++ common/ch_channel_map/test-all.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Helps you compare the results of any changes to ch_channel_map
 #
@@ -9,23 +9,26 @@
 #   ./test-all.sh after
 #   diff -Nru before after
 
-if [ $# -eq 1 ] && [ -d $1 ]; then
-  output_prefix=$1/
+basedir=$(realpath $(dirname $0))
+
+if [[ $# -eq 1 ]]; then
+    mkdir --parents $1
+    output_prefix=$1/
 else
-  output_prefix=
+    output_prefix=
 fi
 
 series=bionic
 for release in queens rocky stein train ussuri; do
-  ./test-channel-map.sh $series $release > ${output_prefix}$series-$release.txt
+    ${basedir}/test-channel-map.sh $series $release > ${output_prefix}$series-$release.txt
 done
 
 series=focal
 for release in ussuri victoria wallaby xena yoga; do
-  ./test-channel-map.sh $series $release > ${output_prefix}$series-$release.txt
+    ${basedir}/test-channel-map.sh $series $release > ${output_prefix}$series-$release.txt
 done
 
 series=jammy
 for release in yoga zed antelope; do
-  ./test-channel-map.sh $series $release > ${output_prefix}$series-$release.txt
+    ${basedir}/test-channel-map.sh $series $release > ${output_prefix}$series-$release.txt
 done
--- common/ch_channel_map/test-channel-map.sh
+++ common/ch_channel_map/test-channel-map.sh
@@ -15,28 +15,28 @@ SCRIPT_DIR=$(dirname $0)
 . ${SCRIPT_DIR}/../charm_lists
 . ${SCRIPT_DIR}/${series}
 if [[ -f ${SCRIPT_DIR}/${series}-${release} ]]; then
-  . ${SCRIPT_DIR}/${series}-${release}
+    . ${SCRIPT_DIR}/${series}-${release}
 fi
 . ${SCRIPT_DIR}/any_series
 
 output=$(mktemp)
 
 for charm in \
-  hacluster \
-  mysql-{router,innodb-cluster} \
-  ovn-{central,chassis,dedicated-chassis} \
-  percona-cluster \
-  rabbitmq-server \
-  vault \
-  barbican-vault \
-  nova-compute \
-  ceph-osd
+    hacluster \
+    mysql-{router,innodb-cluster} \
+    ovn-{central,chassis,dedicated-chassis} \
+    percona-cluster \
+    rabbitmq-server \
+    vault \
+    barbican-vault \
+    nova-compute \
+    ceph-osd
 do
-  if [[ -v CHARM_CHANNEL[${charm}] ]]; then
-    echo "${charm} ${CHARM_CHANNEL[${charm}]}" >> ${output}
-  else
-    echo "${charm} unset" >> ${output}
-  fi
+    if [[ -v CHARM_CHANNEL[${charm}] ]]; then
+        echo "${charm} ${CHARM_CHANNEL[${charm}]}" >> ${output}
+    else
+        echo "${charm} unset" >> ${output}
+    fi
 done
 cat ${output} | column -t
 rm ${output}
