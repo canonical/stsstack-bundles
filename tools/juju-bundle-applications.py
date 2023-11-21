08ed19667f2fdfa142ac69a9e2918603090e478c
Rafael Lopez <rafael.lopez@canonical.com>
2023-11-21 02:16:29 +0000

    Separate relations into separate yaml document
    To adjust to new juju 3.2 behaviour that does not allow relations
    referencing overlay applications in the base bundle.
    
    Ref: https://bugs.launchpad.net/juju/+bug/2041785
    
    Resolves: #149
    Co-authored-by: Nicolas Bock <nicolas.bock@canonical.com>

--- tools/juju-bundle-applications.py
+++ tools/juju-bundle-applications.py
@@ -7,8 +7,8 @@ application_list = set()
 
 for filename in sys.argv[1:]:
     with open(filename, 'r') as f:
-        data = yaml.load(f, Loader=yaml.SafeLoader)
-        if 'applications' in data:
-            application_list.update(data['applications'].keys())
-
+        data = yaml.load_all(f, Loader=yaml.SafeLoader)
+        for d in data:
+            if 'applications' in d:
+                application_list.update(d['applications'].keys())
 print('\n'.join(application_list))
