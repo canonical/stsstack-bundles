19b79aa1c05df47abb5df9f08f0ddbec7cbe952a
Edward Hope-Morley <edward.hope-morley@canonical.com>
2024-10-08 13:40:42 +0100

    Pre-allocate func test vip ports
    Resolves: #269

--- openstack/tools/func_test_tools/common.sh
+++ openstack/tools/func_test_tools/common.sh
@@ -44,3 +44,32 @@ apply_func_test_pr ()
         -f './src/test-requirements*.txt' \
         "$msg"
 }
+
+allocate_port ()
+{
+    # Returns address of port created.
+    #
+    local net_name=$1
+    local port_name=$2
+    local port_id=$(openstack port create --network $net_name $port_name -c id -f value)
+    openstack port show -c fixed_ips $port_id -f yaml| yq .fixed_ips[0].ip_address
+}
+
+create_zaza_vip ()
+{
+    # Allocates a vip ensuring to use existing ones if they exist.
+    #
+    # Returns the address of the vip.
+    #
+    local vip_id=$1
+    # We use the same naming convention as ../tools/allocate_vips.sh to avoid conflicts and re-use
+    # those vips.
+    vip_port_name=ps6-vip-ip$vip_id
+    vip_addr=$(openstack port show -c fixed_ips $vip_port_name -f yaml| yq .fixed_ips[0].ip_address)
+    if [[ $vip_addr = null ]]; then
+        # Pre-allocate ports with addresses used for VIPs so that they don't
+        # collide with the deployment itself.
+        vip_addr=$(allocate_port net_${OS_USERNAME}-psd $vip_port_name)
+    fi
+    echo $vip_addr
+}
