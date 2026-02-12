3a83d22ca4bea9e8bf4e85880dd66be457250b2e
Edward Hope-Morley <edward.hope-morley@canonical.com>
2026-02-12 15:14:25 +0000

    Remove reference dir and add url ignore for medium.com

--- docs/conf.py
+++ docs/conf.py
@@ -228,6 +228,7 @@ linkcheck_ignore = [
     "http://127.0.0.1:8000",
     "https://github.com/canonical/ACME/*",
     "https://github.com/canonical/stsstack-bundles/*",
+    "https://medium.com/*",
     ]
 
 
