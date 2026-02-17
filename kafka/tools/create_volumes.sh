8d400e6bd5afa453fa44bd509f664ffa94f015df
Marcin Wilk <marcin.wilk@canonical.com>
2026-02-17 10:17:07 +0100

    Add all shell scripts to bashate
    This patch also resolves all bashate errors on the added files.
    
    Signed-off-by: Marcin Wilk <marcin.wilk@canonical.com>

--- kafka/tools/create_volumes.sh
+++ kafka/tools/create_volumes.sh
@@ -5,15 +5,15 @@ num=${#machines[@]}
 
 volumeIds=()
 for i in $(seq 1 $((num*2))); do
-	if [ "`openstack volume list --name kafka-vol-$i -c Status -f value`" != "available" ]; then
-		volumeIds+=(`openstack volume create kafka-vol-$i --size 1 -c id -f value`)
-	else
-		volumeIds+=(`openstack volume list --name kafka-vol-$i -c ID -f value`)
-	fi
+    if [ "`openstack volume list --name kafka-vol-$i -c Status -f value`" != "available" ]; then
+        volumeIds+=(`openstack volume create kafka-vol-$i --size 1 -c id -f value`)
+    else
+        volumeIds+=(`openstack volume list --name kafka-vol-$i -c ID -f value`)
+    fi
 done
 
 i=0
 for machine in ${machines[@]}; do
-	openstack server add volume $machine ${volumeIds[$((i++))]}
-	openstack server add volume $machine ${volumeIds[$((i++))]}
+    openstack server add volume $machine ${volumeIds[$((i++))]}
+    openstack server add volume $machine ${volumeIds[$((i++))]}
 done
